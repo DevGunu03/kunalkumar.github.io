e0fa57b69cc6dceb77507b66b91b894531c8845d
Kunal Kumar <kunalkumarultraviolet@gmail.com>
2025-11-10 13:28:37 +0100

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+js
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx,md}'],
+  theme: {
+    extend: {
+      colors: {
+        // default palette A (you can change easily)
+        primary: {
+          DEFAULT: '#4f46e5' // indigo-600
+        },
+        accent: {
+          DEFAULT: '#10b981' // emerald-500
+        }
+      }
+    }
+  },
+  plugins: [],
+}
